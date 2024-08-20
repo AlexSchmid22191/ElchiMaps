75c12f1a70a9b4374b8550ff762d15a5a183b05a
Alex <Alex.Schmid91@gmail.com>
2024-08-20 12:03:45 +0200

    Adds a line scan position selector box, the unit suffix of the box reacts to the selected coordinate system.

--- Interface/ElchMenu/ElchPlotMenu.py
+++ Interface/ElchMenu/ElchPlotMenu.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QPushButton, QRadioButton, QButtonGroup, QVBoxLayout, QLabel
+from PySide6.QtWidgets import QWidget, QPushButton, QRadioButton, QButtonGroup, QVBoxLayout, QLabel, QDoubleSpinBox, QHBoxLayout, QAbstractButton
 
 
 class ElchPlotMenu(QWidget):
@@ -12,11 +12,14 @@ class ElchPlotMenu(QWidget):
                                   for key in ['Angles', 'Reciprocal Vectors']}
         self.coordinate_check_group = QButtonGroup()
         self.coordinate_check_group.setExclusive(True)
+        self.coordinate_checks['Angles'].setChecked(True)
 
-        self.line_checkcs = {key: QRadioButton(parent=self, text=key, objectName=key)
-                             for key in ['Omega', '2 Theta', 'q parallel', 'q normal']}
+        self.line_checks = {key: QRadioButton(parent=self, text=key, objectName=key)
+                            for key in ['Omega', '2 Theta', 'q parallel', 'q normal']}
         self.line_check_group = QButtonGroup()
         self.line_check_group.setExclusive(True)
+
+        self.line_position_box = QDoubleSpinBox(decimals=2, singleStep=1e-2, minimum=0, maximum=90, suffix=u'°')
 
         vbox = QVBoxLayout()
         vbox.addWidget(QLabel(text='Coordinate System', objectName='Header'))
@@ -26,9 +29,16 @@ class ElchPlotMenu(QWidget):
         vbox.addSpacing(20)
 
         vbox.addWidget(QLabel(text='Line Scans', objectName='Header'))
-        for key, button in self.line_checkcs.items():
+        for key, button in self.line_checks.items():
             self.line_check_group.addButton(button)
             vbox.addWidget(button)
+        vbox.addSpacing(10)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(QLabel(text='Line scan position:'))
+        hbox.addWidget(self.line_position_box)
+        vbox.addLayout(hbox)
+
         vbox.addSpacing(20)
 
         vbox.addWidget(QLabel(text='Plotting Control', objectName='Header'))
@@ -39,3 +49,14 @@ class ElchPlotMenu(QWidget):
         vbox.setSpacing(10)
         vbox.setContentsMargins(10, 10, 10, 10)
         self.setLayout(vbox)
+
+        self.coordinate_check_group.buttonClicked.connect(self.change_coordinates)
+
+    def change_coordinates(self, button):
+        match button.objectName():
+            case 'Angles':
+                self.line_position_box.setSuffix('°')
+                # Send a signal to the plot wondows to change units and map
+            case 'Reciprocal Vectors':
+                self.line_position_box.setSuffix(u' Å⁻¹')
+                # Send a signal to the plot wondows to change units and map
